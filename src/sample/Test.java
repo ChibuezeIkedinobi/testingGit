da0a6e191adc6f5f36f9a17e68c98590ee5ed1cb
Ikedinobi <Ikdinobi@gmail.com>
2023-06-30 08:22:32 +0100

    Updated test.java to a new line

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -3,5 +3,6 @@ package sample;
 public class Test {
     public static void main(String[] args) {
         System.out.println("My name is Ikedinobi");
+        System.out.println("This is a sample code");
     }
 }
