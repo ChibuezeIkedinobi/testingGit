be058fff80a62f33afa035efed4c3a4f8b0f3d3e
Ikedinobi <Ikdinobi@gmail.com>
2023-06-30 08:46:33 +0100

    This commit is meant to be in the gitClass-fix branch

--- src/sample/gitClass.java
+++ src/sample/gitClass.java
@@ -3,5 +3,7 @@ package sample;
 public class gitClass {
     public static void main(String[] args) {
         System.out.println("A new class has been created");
+        System.out.println("I changed the branch to gitClass-fix");
+        System.out.println("I just did it so as to master the art of managing a git branch");
     }
 }
